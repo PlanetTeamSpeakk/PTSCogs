88de8978656177c597d0c4396a27c3f0aec9b9d3
PlanetTeamSpeak <tygomourits@ziggo.nl>
2016-12-07 20:01:23 +0100

    [Marry] The bot will now try to make a marriage channel if it's not there

--- marry/marry.py
+++ marry/marry.py
@@ -14,14 +14,6 @@ class marry:
     @commands.command(pass_context=True)
     async def marry(self, ctx, yourlovedone:discord.Member):
         """Now you can finally marry your loved one."""
-        if yourlovedone.id == self.bot.user.id:
-            if ctx.message.author.id != settings.owner:
-                await self.bot.say("I only marry my owner!")
-                return
-            else:
-                pass
-        else:
-            pass
         if yourlovedone.id == ctx.message.author.id:
             await self.bot.say("You can't marry yourself, that would be weird wouldn't it?")
             return
@@ -48,34 +40,23 @@ class marry:
             await self.bot.send_message(yourlovedone, "Your divorce id is `{0}`, don't ever give this to anyone or they can divorce you!\nTo divorce type `{1}divorce {0}`.".format(married_role.id, ctx.prefix))
         else:
             pass
-        marchan = discord.utils.find(lambda c: c.name == 'marriage', ctx.message.server.channels)
+        marchan = discord.utils.find(lambda c: c.name == 'marriage', ctx.message.server.channels, type=discord.ChannelType.text)
         if marchan:
             await self.bot.say("You're now married, congratulations!")
             await self.bot.send_message(marchan, "{} married {} congratulations!".format(ctx.message.author.mention, yourlovedone.mention))
         else:
-            await self.bot.say("{} married {}, congratulations! I suggest telling the server owner or moderators to make a #marriage channel though.".format(ctx.message.author.mention, yourlovedone.mention))
-            return
+            try:
+                marchan = await self.bot.create_channel(ctx.message.server, "marriage", type=discord.ChannelType.text)
+                await self.bot.say("You're now married, congratulations!")
+                await self.bot.send_message(marchan, "{} married {} congratulations!".format(ctx.message.author.mention, yourlovedone.mention))
+            except:
+                await self.bot.say("{} married {}, congratulations! I suggest telling the server owner or moderators to make a #marriage channel though.".format(ctx.message.author.mention, yourlovedone.mention))
+        return
         
     @checks.is_owner()
     @commands.command(pass_context=True)
     async def forcemarry(self, ctx, person:discord.Member, lovedone:discord.Member):
         """Now you can finally marry your loved one."""
-        if lovedone.id == self.bot.user.id:
-            if person.id != settings.owner:
-                await self.bot.say("I only marry my owner!")
-                return
-            else:
-                pass
-        else:
-            pass
-        if person.id == self.bot.user.id:
-            if lovedone.id != settings.owner:
-                await self.bot.say("I only marry my owner!")
-                return
-            else:
-                pass
-        else:
-            pass
         if lovedone.id == person.id:
             await self.bot.say("You can't let someone marry him/herself that would be weird wouldn't it?")
             return
@@ -90,11 +71,17 @@ class marry:
         await self.bot.add_roles(person, married_role)
         await self.bot.add_roles(lovedone, married_role)
         if not person.bot:
-            await self.bot.send_message(person, "**{0}** married you to **{1}**.\nYour divorce id is `{2}`, don't ever give this to anyone or they can divorce you!\nTo divorce type `{3}divorce {2}`.".format(ctx.message.author.name, lovedone.name, married_role.id, ctx.prefix))
+            try:
+                await self.bot.send_message(person, "**{0}** married you to **{1}**.\nYour divorce id is `{2}`, don't ever give this to anyone or they can divorce you!\nTo divorce type `{3}divorce {2}`.".format(ctx.message.author.name, lovedone.name, married_role.id, ctx.prefix))
+            except:
+                pass
         else:
             pass
         if not lovedone.bot:
-            await self.bot.send_message(lovedone, "**{0}** married you to **{1}**.\nYour divorce id is `{2}`, don't ever give this to anyone or they can divorce you!\nTo divorce type `{3}divorce {2}`.".format(ctx.message.author.name, lovedone.name, married_role.id, ctx.prefix))
+            try:
+                await self.bot.send_message(person, "**{0}** married you to **{1}**.\nYour divorce id is `{2}`, don't ever give this to anyone or they can divorce you!\nTo divorce type `{3}divorce {2}`.".format(ctx.message.author.name, lovedone.name, married_role.id, ctx.prefix))
+            except:
+                pass
         else:
             pass
         marchan = discord.utils.find(lambda c: c.name == 'marriage', ctx.message.server.channels)
@@ -102,8 +89,13 @@ class marry:
             await self.bot.say("You're now married, congratulations!")
             await self.bot.send_message(marchan, "{} was forced to marry {}.".format(person.mention, lovedone.mention))
         else:
-            await self.bot.say("{} was forced to marry {}. I suggest telling the server owner or moderators to make a #marriage channel though.".format(person.mention, lovedone.mention))
-            return
+            try:
+                marchan = await self.bot.create_channel(ctx.message.server, "marriage", type=discord.ChannelType.text)
+                await self.bot.say("You're now married, congratulations!")
+                await self.bot.send_message(marchan, "{} was forced to marry {}.".format(person.mention, lovedone.mention))
+            except:
+                await self.bot.say("{} married {}, congratulations! I suggest telling the server owner or moderators to make a #marriage channel though.".format(person.mention, lovedone.mention))
+        return
         
     @commands.command(pass_context=True)
     async def divorce(self, ctx, divorce_id):
@@ -121,8 +113,13 @@ class marry:
                 await self.bot.say("You're now divorced.")
                 await self.bot.send_message(marchan, "{} divorced ID `{}`.".format(ctx.message.author.mention, divorce_id))
             else:
-                await self.bot.say("You're now divorced! I suggest telling the server owner or moderators to make a #marriage channel though.")
-                return
+                try:
+                    marchan = await self.bot.create_channel(ctx.message.server, "marriage", type=discord.ChannelType.text)
+                    await self.bot.say("You're now divorced.")
+                    await self.bot.send_message(marchan, "{} divorced ID `{}`.".format(ctx.message.author.mention, divorce_id))
+                except:
+                    await self.bot.say("You're now divorced! I suggest telling the server owner or moderators to make a #marriage channel though.")
+                    return
         except discord.Forbidden:
             await self.bot.say("I do not have the `manage roles` permission, I need it to divorce you.")
             return
