cedca73dbfebe5d0253c5204ae973d03971ccc38
PlanetTeamSpeak <tygomourits@ziggo.nl>
2016-12-08 18:54:59 +0100

    [Warner] Fixed the not loading issue

--- warner/warner.py
+++ warner/warner.py
@@ -4,59 +4,58 @@ from cogs.utils.dataIO import dataIO
 import os
 from .utils import checks
 
-warnings = dataIO.load_json("data/warner/warnings.json")
-
 class Warner:
     """Warn people."""
 
     def __init__(self, bot):
         self.bot = bot
-
+        self.warnings = dataIO.load_json("data/warner/warnings.json")
+        
     @checks.mod_or_permissions()
     @commands.command(pass_context=True)
     async def warn(self, ctx, user:discord.Member):
         """Warn people for their actions."""
-        print(warnings)
+        print(self.warnings)
         serverid = ctx.message.server.id
         userid = user.id
-        if serverid not in warnings:
-            warnings[serverid] = {}
-            dataIO.save_json("data/warner/warnings.json", warnings)
-        if userid not in warnings[serverid]:
-            warnings[serverid][userid] = 1
-            dataIO.save_json("data/warner/warnings.json", warnings)
-            await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warnings for this user.".format(warnings[serverid][userid]))
+        if serverid not in self.warnings:
+            self.warnings[serverid] = {}
+            dataIO.save_json("data/warner/warnings.json", self.warnings)
+        if userid not in self.warnings[serverid]:
+            self.warnings[serverid][userid] = 1
+            dataIO.save_json("data/warner/warnings.json", self.warnings)
+            await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warning(s) for this user.".format(self.warnings[serverid][userid]))
             return
         else:
-            warnings[serverid][userid] = warnings[serverid][userid] + 1
-            dataIO.save_json("data/warner/warnings.json", warnings)
-            if warnings[serverid][userid] == 3:
-                del warnings[serverid][userid]
-                dataIO.save_json("data/warner/warnings.json", warnings)
+            self.warnings[serverid][userid] = self.warnings[serverid][userid] + 1
+            dataIO.save_json("data/warner/warnings.json", self.warnings)
+            if self.warnings[serverid][userid] == 3:
+                del self.warnings[serverid][userid]
+                dataIO.save_json("data/warner/warnings.json", self.warnings)
                 await self.bot.say("User has 3 warnings, what are you consequences? (btw, warnings are reset for this user!)")
                 return
-            if warnings[serverid][userid] > 3:
-                await self.bot.say("User has over 3 warnings, maybe forgot to reset?")
+            if self.warnings[serverid][userid] > 3:
+                await self.bot.say("User has over 3 warnings, wut?")
                 return
             else:
-                await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warnings for this user.".format(warnings[serverid][userid]))
+                await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warnings for this user.".format(self.warnings[serverid][userid]))
                 return
                
     @checks.mod_or_permissions()
     @commands.command(pass_context=True)
     async def resetwarns(self, ctx, user:discord.Member):
-        """Reset the warnings you gave to someone"""
+        """Reset the self.warnings you gave to someone"""
         serverid = ctx.message.server.id
         userid = user.id
-        if serverid not in warnings:
+        if serverid not in self.warnings:
             await self.bot.say("No one in this server has got a warning yet.")
             return
-        elif userid not in warnings[serverid]:
+        elif userid not in self.warnings[serverid]:
             await self.bot.say("This user doesn't have a warning yet.")
             return
         else:
-            del warnings[serverid][userid]
-            dataIO.save_json("data/warner/warnings.json", warnings)
+            del self.warnings[serverid][userid]
+            dataIO.save_json("data/warner/warnings.json", self.warnings)
             await self.bot.say("Users warnings succesfully reset!")
             return
                
