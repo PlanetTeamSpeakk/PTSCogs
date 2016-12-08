ddca04b401aed0a16eb30dbedf2370d19ee88ab5
PlanetTeamSpeak <tygomourits@ziggo.nl>
2016-12-08 18:10:14 +0100

    [Warner] Added the warner cog

---
+++ warner/warner.py
@@ -0,0 +1,76 @@
+import discord
+from discord.ext import commands
+from cogs.utils.dataIO import dataIO
+import os
+from .utils import checks
+
+warnings = dataIO.load_json("data/warner/warnings.json")
+
+class Warner:
+    """Warn people."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @checks.mod_or_permissions()
+    @commands.command(pass_context=True)
+    async def warn(self, ctx, user:discord.Member):
+        """Warn people for their actions."""
+        print(warnings)
+        serverid = ctx.message.server.id
+        userid = user.id
+        if serverid not in warnings:
+            warnings[serverid] = {}
+            dataIO.save_json("data/warner/warnings.json", warnings)
+        if userid not in warnings[serverid]:
+            warnings[serverid][userid] = 1
+            dataIO.save_json("data/warner/warnings.json", warnings)
+            await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warnings for this user.".format(warnings[serverid][userid]))
+            return
+        else:
+            warnings[serverid][userid] = warnings[serverid][userid] + 1
+            dataIO.save_json("data/warner/warnings.json", warnings)
+            if warnings[serverid][userid] == 3:
+                del warnings[serverid][userid]
+                dataIO.save_json("data/warner/warnings.json", warnings)
+                await self.bot.say("User has 3 warnings, what are you consequences? (btw, warnings are reset for this user!)")
+                return
+            if warnings[serverid][userid] > 3:
+                await self.bot.say("User has over 3 warnings, maybe forgot to reset?")
+                return
+            else:
+                await self.bot.say("1 warning has been added for the user.\nThat makes a total of {} warnings for this user.".format(warnings[serverid][userid]))
+                return
+               
+    @checks.mod_or_permissions()
+    @commands.command(pass_context=True)
+    async def resetwarns(self, ctx, user:discord.Member):
+        """Reset the warnings you gave to someone"""
+        serverid = ctx.message.server.id
+        userid = user.id
+        if serverid not in warnings:
+            await self.bot.say("No one in this server has got a warning yet.")
+            return
+        elif userid not in warnings[serverid]:
+            await self.bot.say("This user doesn't have a warning yet.")
+            return
+        else:
+            del warnings[serverid][userid]
+            dataIO.save_json("data/warner/warnings.json", warnings)
+            await self.bot.say("Users warnings succesfully reset!")
+            return
+               
+def check_folders():
+    if not os.path.exists("data/warner"):
+        print("Creating data/warner folder...")
+        os.makedirs("data/warner")
+
+def check_files():
+    if not os.path.exists("data/warner/warnings.json"):
+        print("Creating data/warner/warnings.json file...")
+        dataIO.save_json("data/warner/warnings.json", {})
+        
+def setup(bot):
+    check_folders()
+    check_files()
+    bot.add_cog(Warner(bot))
