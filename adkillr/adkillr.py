cdda4f44eb2360e68c7a4a134f5ebbff5fba4fff
PlanetTeamSpeak <tygomourits@ziggo.nl>
2016-12-08 19:10:12 +0100

    [Adkillr] Fixed the not loading issue

--- adkillr/adkillr.py
+++ adkillr/adkillr.py
@@ -5,69 +5,63 @@ from __main__ import send_cmd_help, settings
 from cogs.utils.dataIO import dataIO
 import os
 
-adkillr = dataIO.load_json("data/adkillr/adkillr.json")
+
 
 class Adkillr:
     """Adkillr"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.adkillr = dataIO.load_json("data/adkillr/adkillr.json")
         
     async def adkill(self, message):
         """Kill them ads!"""
         serverid = message.server.id
         ad = message
-        adkillr = ""
-        adkillr = dataIO.load_json("data/adkillr/adkillr.json")
+        self.adkillr = ""
+        self.adkillr = dataIO.load_json("data/adkillr/adkillr.json")
         if ad.server is None:
             pass
         try:
-            temp = adkillr[serverid]['toggle']
+            temp = self.adkillr[serverid]['toggle']
         except:
-            adkillr[serverid] = {'toggle': True, 'message': "{0.mention} don't send links!"}
-            dataIO.save_json("data/adkillr/adkillr.json", adkillr)
-        roles = [r.name for r in ad.author.roles]
-        bot_admin = settings.get_server_admin(message.server)
-        bot_mod = settings.get_server_mod(message.server)
-        if bot_admin in roles:
-            return
-        elif bot_mod in roles:
-            return
+            self.adkillr[serverid] = {'toggle': True, 'message': "{0.mention} don't send links!"}
+            dataIO.save_json("data/adkillr/adkillr.json", self.adkillr)
         if ad.author.id == settings.owner:
             return
         elif ad.author.permissions_in(ad.channel).manage_messages:
             return
-        if adkillr[serverid]['toggle'] is True:
+        if self.adkillr[serverid]['toggle'] is True:
             if "http://" in ad.content.lower() or "https://" in ad.content.lower():
                 if "." in ad.content.lower():
                     if self.bot.user.permissions_in(ad.channel).manage_messages:
                         await self.bot.delete_message(ad)
-                        await self.bot.send_message(message.channel, adkillr[ad.server.id]['message'].format(ad.author))
+                        await self.bot.send_message(message.channel, self.adkillr[ad.server.id]['message'].format(ad.author))
                     else:
                         pass
 
-    @commands.group(name = "adkillr", pass_context=True, no_pm=True)
+    @commands.group(name="adkillr", pass_context=True, no_pm=True)
     async def _adkillr(self, ctx):
         """Manages the settings for Adkillr."""
         serverid = ctx.message.server.id
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
-        if serverid not in adkillr:
-            adkillr[serverid] = {'toggle': True, 'message': "{0.mention} don't send links!"}
-            dataIO.save_json("data/adkillr/adkillr.json", adkillr)
+        if serverid not in self.adkillr:
+            self.adkillr[serverid] = {'toggle': True, 'message': "{0.mention} don't send links!"}
+            dataIO.save_json("data/adkillr/adkillr.json", self.adkillr)
 
     @_adkillr.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions()
     async def toggle(self, ctx):
         """Enable/disables Adkillr in the server."""
         serverid = ctx.message.server.id
-        if adkillr[serverid]['toggle'] is True:
-            adkillr[serverid]['toggle'] = False
+        if self.adkillr[serverid]['toggle'] is True:
+            self.adkillr[serverid]['toggle'] = False
             await self.bot.say('Adkillr is now disabled')
-        elif adkillr[serverid]['toggle'] is False:
-            adkillr[serverid]['toggle'] = True
+        elif self.adkillr[serverid]['toggle'] is False:
+            self.adkillr[serverid]['toggle'] = True
             await self.bot.say('Adkillr is now enabled')
-        dataIO.save_json("data/adkillr/adkillr.json", adkillr)
+        dataIO.save_json("data/adkillr/adkillr.json", self.adkillr)
         
     @_adkillr.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions()
@@ -76,8 +70,8 @@ class Adkillr:
         {0} will turn into the authors name, you can use {0.name}, {0.mention} or {0.id}.
         Default is {0.mention} don't send links!"""
         serverid = ctx.message.server.id
-        adkillr[serverid]['message'] = message
-        dataIO.save_json("data/adkillr/adkillr.json", adkillr)
+        self.adkillr[serverid]['message'] = message
+        dataIO.save_json("data/adkillr/adkillr.json", self.adkillr)
         await self.bot.say("Message set!")
         
 def check_folders():
