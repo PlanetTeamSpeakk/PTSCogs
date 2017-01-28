da3b4192a9f1db6ff9ab45031f45f3be2da719ab
PlanetTeamSpeak <tygomourits@ziggo.nl>
2017-01-28 13:47:58 +0100

    [Memes] Complete rewrite?
    Memes are no longer global but per server and you can toggle the reactions.

--- memes/memes.py
+++ memes/memes.py
@@ -77,54 +77,73 @@ class Memes:
 
     def __init__(self, bot):
         self.bot = bot
-        self.memelist = dataIO.load_json("data/memes/memes.json")
+        self.settings = dataIO.load_json("data/memes/settings.json")
         self.airhorn = [
         "data/memes/airhorns/airhorn1.mp3",
         "data/memes/airhorns/airhorn2.mp3",
         "data/memes/airhorns/airhorn3.mp3"
         ]
+        self.yesno = [
+        "yes. https://media.giphy.com/media/l46CabMtEkqUtrzkA/giphy.gif", 
+        "yes. https://media.giphy.com/media/l3vRhtXnCLgypqh7a/giphy.gif", 
+        "yes. https://media.giphy.com/media/l3vR3ACyHLgbOIjZe/source.gif", 
+        "no. https://media.giphy.com/media/3oz8xM4Qy4IVCelqZq/source.gif", 
+        "no. https://media.giphy.com/media/KaXENSCPjqnK0/giphy.gif", 
+        "no. https://media.giphy.com/media/T5QOxf0IRjzYQ/giphy.gif"
+        ]
         
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
     async def meme(self, ctx):
         """Shows a random meme."""
-        await self.bot.send_message(ctx.message.channel, choice(self.memelist))
+        if ctx.message.server.id not in self.settings:
+            self.settings[ctx.message.server.id] = {'memes': memelist, 'disabled': False}
+            self.save_settings()
+        await self.bot.say(choice(self.settings[ctx.message.server.id]['memes']))
         
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
     async def addmeme(self, ctx, memelink_imgurpls):
-        """Adds a meme to the global list of memes."""
+        """Adds a meme to the list of memes of this server."""
         memelink = memelink_imgurpls
         if memelink.startswith("http://i.imgur.com/"):
-            self.memelist.append(memelink + " by {}.".format(str(ctx.message.author)))
-            dataIO.save_json("data/memes/memes.json", self.memelist)
+            if ctx.message.server.id not in self.settings:
+                self.settings[ctx.message.server.id] = {'memes': memelist, 'disabled': False}
+            self.settings[ctx.message.server.id]['memes'].append(memelink + " by {}.".format(str(ctx.message.author)))
+            self.save_settings()
             await self.bot.say("Meme added!")
         else:
             await self.bot.say("Memelink was not an imgur link, an example imgur link would be: <http://i.imgur.com/OyNz2uG.png>")
         
-    @commands.command()
+    @commands.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions()
-    async def delmeme(self, memelink_and_owner):
+    async def delmeme(self, ctx, *, memelink):
         """Deletes a meme.
         
         Example:
-        [p]delmeme "http://i.imgur.com/OyNz2uG.png by PlanetTeamSpeak#4157"
+        [p]delmeme http://i.imgur.com/OyNz2uG.png
         """
-        memelink = memelink_and_owner
-        try:
-            self.memelist.remove(memelink)
-            dataIO.save_json("data/memes/memes.json", self.memelist)
+        if ctx.message.server.id not in self.settings:
+            await self.bot.say("This server has no memes to delete.")
+            return
+        found = False
+        for meme in self.settings[ctx.message.server.id]['memes']:
+            if memelink in meme:
+                found = True
+                memelink = meme
+        if not found:
+            await self.bot.say("That's not a valid meme.")
+        else:
+            self.settings[ctx.message.server.id]['memes'].remove(memelink)
+            self.save_settings()
             await self.bot.say("Meme removed!")
-        except:
-            await self.bot.say("Couldn't delete the meme from memes.json, was the format correct? A correct format would be \n\"http://i.imgur.com/OyNz2uG.png by PlanetTeamSpeak#4157\"")
 		
     @commands.command()
     async def goodshit(self):
         """Good shit"""
         await self.bot.say("sign me the FUCK up :ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes: good shit go౦ԁ sHit:ok_hand: thats :heavy_check_mark: some good:ok_hand::ok_hand:shit right:ok_hand::ok_hand:there:ok_hand::ok_hand::ok_hand: right:heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do ƽaү so my self :100: i say so :100: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:100: :ok_hand::ok_hand: :ok_hand:НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand::ok_hand:Good shit\nhttps://www.youtube.com/watch?v=OYjv8ogsEGE&ab_channel=HeadStriker",)
 
-    @commands.command()
-    async def yesno(self):
+    @commands.command(name="yesno")
+    async def _yesno(self):
         """Says yes or no."""
-        self.yesno = ["yes. https://media.giphy.com/media/l46CabMtEkqUtrzkA/giphy.gif", "yes. https://media.giphy.com/media/l3vRhtXnCLgypqh7a/giphy.gif", "yes. https://media.giphy.com/media/l3vR3ACyHLgbOIjZe/source.gif", "no. https://media.giphy.com/media/3oz8xM4Qy4IVCelqZq/source.gif", "no. https://media.giphy.com/media/KaXENSCPjqnK0/giphy.gif", "no. https://media.giphy.com/media/T5QOxf0IRjzYQ/giphy.gif"]
         await self.bot.say("I say " + choice(self.yesno))
         
     @commands.command(pass_context=True)
@@ -158,7 +177,7 @@ class Memes:
         else:
             await self.bot.send_file(ctx.message.channel, fp="data/memes/datboi.png", filename="datboi.png")
             
-    @commands.command(pass_context=True, name="airhorn")
+    @commands.command(pass_context=True, name="airhorn", no_pm=True)
     async def _airhorn(self, ctx):
         """Plays an airhorn in the voice channel you're in."""
         self.ah1Loaded = os.path.exists('data/memes/airhorns/airhorn1.mp3')
@@ -174,7 +193,7 @@ class Memes:
                 print(e)
                 print("Memes error, couldn't download the airhorn sounds, I suggest disabling the airhorn command.")
                 return
-        elif not self.ah2Loaded:
+        if not self.ah2Loaded:
             try:
                 async with aiohttp.get("https://raw.githubusercontent.com/PlanetTeamSpeakk/PTSCogs/master/airhorns/airhorn2.mp3") as r:
                     ah1 = await r.content.read()
@@ -184,7 +203,7 @@ class Memes:
                 print(e)
                 print("Memes error, couldn't download the airhorn sounds, I suggest disabling the airhorn command.")
                 return
-        elif not self.ah3Loaded:
+        if not self.ah3Loaded:
             try:
                 async with aiohttp.get("https://raw.githubusercontent.com/PlanetTeamSpeakk/PTSCogs/master/airhorns/airhorn3.mp3") as r:
                     ah1 = await r.content.read()
@@ -245,23 +264,23 @@ class Memes:
             except:
                 return
                 
-    @commands.group(name="airhornsong", pass_context=True)
+    @commands.group(name="airhornsong", pass_context=True, no_pm=True)
     async def _airhornsong(self, ctx):
         """Some air horn songs."""
         if not ctx.invoked_subcommand:
             await send_cmd_help(ctx)
             
-    @_airhornsong.command()
+    @_airhornsong.command(no_pm=True)
     async def credits(self):
         """Some credits to the song makers."""
         await self.bot.say("All songs are made by Cyranek \n<https://www.youtube.com/channel/UCMYTaTc_gVRyGF6LWzdIsqA>")
         
-    @_airhornsong.command()
+    @_airhornsong.command(no_pm=True)
     async def list(self):
         """Shows you the current list of airhorn songs."""
         await self.bot.say("You can currently choose from:\nletitgo\nturndownforwhat\ndarudesandstorm\nsonic")
             
-    @_airhornsong.command(pass_context=True)
+    @_airhornsong.command(pass_context=True, no_pm=True)
     async def letitgo(self, ctx):
         """Plays let it go airhorn version."""
         self.letitgoLoaded = os.path.exists("data/memes/airhornsongs/letitgo.mp3")
@@ -306,7 +325,7 @@ class Memes:
             await self.bot.say("You're not in a voice channel.")
             return
                 
-    @_airhornsong.command(pass_context=True)
+    @_airhornsong.command(pass_context=True, no_pm=True)
     async def turndownforwhat(self, ctx):
         """Plays the turn down for what song airhorn version."""
         self.turndownforwhatLoaded = os.path.exists("data/memes/airhornsongs/turndownforwhat.mp3")
@@ -351,7 +370,7 @@ class Memes:
             await self.bot.say("You're not in a voice channel.")
             return
                 
-    @_airhornsong.command(pass_context=True)
+    @_airhornsong.command(pass_context=True, no_pm=True)
     async def darudesandstorm(self, ctx):
         """Plays the darude sandstorm song airhorn version."""
         self.darudesandstormLoaded = os.path.exists("data/memes/airhornsongs/darudesandstorm.mp3")
@@ -396,7 +415,7 @@ class Memes:
             await self.bot.say("You're not in a voice channel.")
             return
                 
-    @_airhornsong.command(pass_context=True)
+    @_airhornsong.command(pass_context=True, no_pm=True)
     async def sonic(self, ctx):
         """Plays sonic song airhorn version."""
         self.sonicLoaded = os.path.exists("data/memes/airhornsongs/sonic.mp3")
@@ -441,7 +460,7 @@ class Memes:
             await self.bot.say("You're not in a voice channel.")
             return
             
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
     async def sadpiano(self, ctx):
         """Plays sad piano for you."""
         self.sadpianoLoaded = os.path.exists("data/memes/sadpiano.mp3")
@@ -498,10 +517,24 @@ class Memes:
         else:
             await self.bot.say("http://i.imgur.com/HQGh7tL.gif")
         
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.mod_or_permissions()
+    async def togglereactions(self, ctx):
+        """Disables reactions like "ayy" and "oh shit\""""
+        if ctx.message.server.id not in self.settings:
+            self.settings[ctx.message.server.id] = {'memes': memelist, 'disabled': False}
+        if not self.settings[ctx.message.server.id]['disabled']:
+            self.settings[ctx.message.server.id]['disabled'] = True
+            await self.bot.say("Bot will no longer respond to \"ayy\" and \"oh shit\".")
+        else:
+            self.settings[ctx.message.server.id]['disabled'] = False
+            await self.bot.say("Bot will once again respond to \"ayy\" and \"oh shit\".")
+        self.save_settings()
+        
     async def memes(self, message):
         if message.server != None:
             if not "bots" in message.server.name.lower():
-                if "ayy" in message.content.lower():
+                if (message.server.id in self.settings) and not (self.settings[message.server.id]['disabled']) and ("ayy" in message.content.lower()):
                     self.lmaoLoaded = os.path.exists('data/memes/maolmao.png')
                     if not self.lmaoLoaded:
                         try:
@@ -582,6 +615,9 @@ class Memes:
                     else:
                         await self.bot.send_file(message.channel, fp="data/memes/feelsbadman.png", filename="feelsbadman.png")
             
+    def save_settings(self):
+        dataIO.save_json("data/memes/settings.json", self.settings)
+            
 def check_folders():
     if not os.path.exists("data/memes"):
         print("Creating data/memes folder...")
@@ -594,9 +630,9 @@ def check_folders():
         os.makedirs("data/memes/airhornsongs")
         
 def check_files():
-    if not os.path.exists("data/memes/memes.json"):
-        print("Creating data/memes/memes.json file...")
-        dataIO.save_json("data/memes/memes.json", memelist)
+    if not os.path.exists("data/memes/settings.json"):
+        print("Creating data/memes/settings.json file...")
+        dataIO.save_json("data/memes/settings.json", {})
         
 def setup(bot):
     check_folders()
