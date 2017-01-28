d0bb77fc5531bba15ddbaf789d5681e5df73f46f
PlanetTeamSpeak <tygomourits@ziggo.nl>
2017-01-28 21:29:28 +0100

    [Autorole] Nvm

--- autorole/autorole.py
+++ autorole/autorole.py
@@ -15,34 +15,34 @@ class Autorole:
     @checks.admin_or_permissions()
     async def autorole(self, ctx):
         """Manage autorole settings."""
-        if ctx.message.server.id not in self.settings:
-            self.settings[ctx.message.server.id] = {'ROLE': None, 'TOGGLED': False}
-            self.save_settings()
         if not ctx.invoked_subcommand:
             await self.bot.send_cmd_help(ctx)
-            await self.bot.say("```Role: {}\nDisabled: {}```".format(self.settings[ctx.message.server.id]['ROLE'], self.settings[ctx.message.server.id]['TOGGLED']))
+            await self.bot.say("```Role: {}\nDisabled: {}```".format(self.settings[ctx.message.server.id]['role'], self.settings[ctx.message.server.id]['toggled']))
+        if ctx.message.server.id not in self.settings:
+            self.settings[ctx.message.server.id] = {'role': None, 'toggled': False}
+            self.save_settings()
 
     @autorole.command(pass_context=True)
     async def setrole(self, ctx, role:discord.Role):
         """Set the role the bot should assign on join, the highest role that the bot has should be higher than this one."""
-        self.settings[ctx.message.server.id]['ROLE'] = role.id
+        self.settings[ctx.message.server.id]['role'] = role.id
         self.save_settings()
         await self.bot.say("Role has been set.")
         
     @autorole.command(pass_context=True)
     async def toggle(self, ctx):
         """Toggle if the bot should assign a role on member join."""
-        if not self.settings[ctx.message.server.id]['TOGGLED']:
-            self.settings[ctx.message.server.id]['TOGGLED'] = True
+        if not self.settings[ctx.message.server.id]['toggled']:
+            self.settings[ctx.message.server.id]['toggled'] = True
             await self.bot.say("The bot will once again assign a role on member join.")
         else:
-            self.settings[ctx.message.server.id]['TOGGLED'] = True
+            self.settings[ctx.message.server.id]['toggled'] = True
             await self.bot.say("The bot will no longer assign a role on member join.")
         self.save_settings()
         
     async def on_member_join(self, member):
-        if (member.server.id in self.settings) and (self.settings[member.server.id]['ROLE'] != None):
-            role = discord.utils.get(member.server.roles, id=self.settings[member.server.id]['ROLE'])
+        if (member.server.id in self.settings) and (self.settings[member.server.id]['role'] != None):
+            role = discord.utils.get(member.server.roles, id=self.settings[member.server.id]['role'])
             if role != None:
                 try:
                     await self.bot.add_roles(member, role)
