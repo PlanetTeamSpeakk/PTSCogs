1a808c810973a681964e4627c28e2ab778a186a5
PlanetTeamSpeak <tygomourits@ziggo.nl>
2017-02-06 21:09:54 +0100

    [Memes] Steam sales :D

--- memes/memes.py
+++ memes/memes.py
@@ -161,6 +161,11 @@ class Memes:
         """Good shit"""
         await self.bot.say("sign me the FUCK up :ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes: good shit go౦ԁ sHit:ok_hand: thats :heavy_check_mark: some good:ok_hand::ok_hand:shit right:ok_hand::ok_hand:there:ok_hand::ok_hand::ok_hand: right:heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do ƽaү so my self :100: i say so :100: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:100: :ok_hand::ok_hand: :ok_hand:НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand::ok_hand:Good shit\nhttps://www.youtube.com/watch?v=OYjv8ogsEGE&ab_channel=HeadStriker",)
 
+    @commands.command()
+    async def sale(self):
+        """Steam sales :D"""
+        await self.bot.say("")
+        
     @commands.command(name="yesno")
     async def _yesno(self):
         """Says yes or no."""
