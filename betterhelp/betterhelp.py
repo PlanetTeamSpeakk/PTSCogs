f8163b0004c66751322ec3470d0d38c56c0f41de
PlanetTeamSpeak <tygomourits@ziggo.nl>
2017-02-18 12:16:25 +0100

    [Betterhelp] Bugfixes

--- betterhelp/betterhelp.py
+++ betterhelp/betterhelp.py
@@ -44,7 +44,7 @@ class BetterHelp:
             if not ctx.message.channel.is_private:
                 await self.bot.edit_message(msg, "I've sent you help in dms!")
         else:
-            ctx.prefix = ctx.prefix.replace("\\", "\\\\") # for my own bot, Impulse Beta (private)
+            ctx.prefix = ctx.prefix.replace("\\\\", "\\\\\\\\") # for my own bot, Impulse Beta (private)
             for cmd in self.bot.commands.keys():
                 if cmd == command.split()[0]:
                     if len(command.split()) == 2:
@@ -64,6 +64,13 @@ class BetterHelp:
                             if (str(param) != "self") and (str(param) != "ctx"):
                                 for attr in dir(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name]):
                                     if command.split()[1].lower() in attr:
+                                        while not hasattr(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name].__getattribute__(attr), "params"):
+                                            for attr1 in dir(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name]):
+                                                if hasattr(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name].__getattribute__(attr1), "params") and command.split()[1] in attr1:
+                                                    attr = attr1
+                                                    break
+                                        print(dict(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name].__getattribute__(attr).params))
+                                        print(attr)
                                         if dict(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name].__getattribute__(attr).params)[param].default != None: # For if it's optional ;), finally found a way.
                                             params.append("<" + param + ">")
                                         else:
@@ -100,7 +107,7 @@ class BetterHelp:
                                         for param in paramsCopy:
                                             if (str(param) != "self") and (str(param) != "ctx"):
                                                 for attr in dir(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name]):
-                                                    if command.split()[2].lower() in attr:
+                                                    if command.split()[2].lower() in attr and hasattr(self.bot.cogs[self.bot.commands[command].cog_name].__getattribute__(attr), "params"):
                                                         if dict(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name].__getattribute__(attr).params)[param].default != None: # For if it's optional ;), finally found a way.
                                                             params.append("<" + param + ">")
                                                         else:
@@ -130,12 +137,13 @@ class BetterHelp:
                         params = []
                         for param in paramsCopy:
                             if (str(param) != "self") and (str(param) != "ctx"): # the list will turn into a NoneType if you remove the 'self' param with params.remove("self")
-                                for attr in dir(self.bot.cogs[self.bot.commands[command.split()[0]].cog_name]):
-                                    if command.lower() in attr:
-                                        if dict(self.bot.cogs[self.bot.commands[command].cog_name].__getattribute__(command).params)[param].default != None: # For if it's optional ;), finally found a way.
+                                for attr in dir(self.bot.cogs[self.bot.commands[command].cog_name]):
+                                    if command.lower() in attr and hasattr(self.bot.cogs[self.bot.commands[command].cog_name].__getattribute__(attr), "params"):
+                                        if dict(self.bot.cogs[self.bot.commands[command].cog_name].__getattribute__(attr).params)[param].default != None: # For if it's optional ;), finally found a way.
                                             params.append("<" + param + ">")
                                         else:
                                             params.append("[" + param + "]")
+                                        break
                         help = self.bot.commands[command].help
                         if params != []:
                             if subcommands != []:
