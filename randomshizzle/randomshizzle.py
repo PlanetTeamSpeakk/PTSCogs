5d37de3d55efd8530b24bc3d1578abb6d5ca5f69
PlanetTeamSpeak <tygomourits@ziggo.nl>
2017-03-12 12:53:28 +0100

    [Randomshizzle] Made some changes

--- randomshizzle/randomshizzle.py
+++ randomshizzle/randomshizzle.py
@@ -13,46 +13,6 @@ class Randomshizzle:
     def __init__(self, bot):
         self.bot = bot
         self.pressfcount = dataIO.load_json("data/pressf/pressf.json")
-        
-    @commands.command()
-    async def cooldog(self):
-        await self.bot.say("╭━━━━╮               This is cooldog. Help \n╰┃ ┣▇━▇                cooldog take over \n ┃ ┃  ╰━▅╮ Discord by pasting\n ╰┳╯ ╰━━┳╯        him in 10 other\n  ╰╮ ┳━━╯            servers or you\n ▕▔▋ ╰╮╭━╮   will never be a cool dog.\n╱▔╲▋╰━┻┻╮╲╱▔▔▔╲\n▏  ▔▔▔▔▔▔▔  O O┃ \n╲╱▔╲▂▂▂▂╱▔╲▂▂▂╱\n ▏╳▕▇▇▕ ▏╳▕▇▇▕\n ╲▂╱╲▂╱ ╲▂╱╲▂╱")
-
-    @commands.command()
-    async def soundsfromspace(self):
-        await self.bot.say("Ooh, spooky. \n<https://github.audio/>")
-
-    @commands.command(pass_context=True)
-    async def flipuser(self, ctx, user : discord.Member):
-        """Flips a user."""
-        if user != None:
-            msg = ""
-            if user.id == self.bot.user.id:
-                user = ctx.message.author
-                msg = "Nice try. You think this is funny? How about *this* instead:\n\n"
-            char = "abcdefghijklmnopqrstuvwxyz"
-            tran = "ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz"
-            table = str.maketrans(char, tran)
-            name = user.display_name.translate(table)
-            char = char.upper()
-            tran = "∀qƆpƎℲפHIſʞ˥WNOԀQᴚS┴∩ΛMX⅄Z"
-            table = str.maketrans(char, tran)
-            name = name.translate(table)
-            await self.bot.say(msg + "(╯°□°）╯︵ " + name[::-1])
-			
-    @commands.command(pass_context=True)
-    async def flipitem(self, ctx, item):
-        """Flips a item."""
-        msg = ""
-        char = "abcdefghijklmnopqrstuvwxyz"
-        tran = "ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz"
-        table = str.maketrans(char, tran)
-        name = item.translate(table)
-        char = char.upper()
-        tran = "∀qƆpƎℲפHIſʞ˥WNOԀQᴚS┴∩ΛMX⅄Z"
-        table = str.maketrans(char, tran)
-        name = name.translate(table)
-        await self.bot.say(msg + "(╯°□°）╯︵ " + name[::-1])
 			
     @commands.command()
     async def punch(self, user : discord.Member):
@@ -67,7 +27,7 @@ class Randomshizzle:
     @commands.command()
     async def triggered(self):
         """TRIGGERED"""
-        await self.bot.say("***__TRIGGERED__***\nhttps://media.giphy.com/media/ZEVc9uplCUJFu/giphy.gif")
+        await self.bot.say("***__TRIGGERED__***\nhttps://cdn.impulsebot.com/ESV2XYIhcF.gif")
 
     @commands.command(pass_context=True)
     async def kys(self, ctx, user : discord.Member = None):
@@ -139,32 +99,32 @@ class Randomshizzle:
     @commands.command()
     async def sombra(self):
         """Sombra."""
-        await self.bot.say("```fix\n"
-        "                       :PB@Bk:                         \n"
-        "                   ,jB@@B@B@B@BBL.                     \n"
-        "                7G@B@B@BMMMMMB@B@B@Nr                  \n"
-        "            :kB@B@@@MMOMOMOMOMMMM@B@B@B1,              \n"
-        "        :5@B@B@B@BBMMOMOMOMOMOMOMM@@@B@B@BBu.          \n"
-        "     70@@@B@B@B@BXBBOMOMOMOMOMOMMBMPB@B@B@B@B@Nr       \n"
-        "   G@@@BJ iB@B@@  OBMOMOMOMOMOMOM@2  B@B@B. EB@B@S     \n"
-        "   @@BM@GJBU.  iSuB@OMOMOMOMOMOMM@OU1:  .kBLM@M@B@     \n"
-        "   B@MMB@B       7@BBMMOMOMOMOMOBB@:       B@BMM@B     \n"
-        "   @@@B@B         7@@@MMOMOMOMM@B@:         @@B@B@     \n"
-        "   @@OLB.          BNB@MMOMOMM@BEB          rBjM@B     \n"
-        "   @@  @           M  OBOMOMM@q  M          .@  @@     \n"
-        "   @@OvB           B:u@MMOMOMMBJiB          .BvM@B     \n"
-        "   @B@B@J         0@B@MMOMOMOMB@B@u         q@@@B@     \n"
-        "   B@MBB@v       G@@BMMMMMMMMMMMBB@5       F@BMM@B     \n"
-        "   @BBM@BPNi   LMEB@OMMMM@B@MMOMM@BZM7   rEqB@MBB@     \n"
-        "   B@@@BM  B@B@B  qBMOMB@B@B@BMOMBL  B@B@B  @B@B@M     \n"
-        "    J@@@@PB@B@B@B7G@OMBB.   ,@MMM@qLB@B@@@BqB@BBv      \n"
-        "       iGB@,i0@M@B@MMO@E  :  M@OMM@@@B@Pii@@N:         \n"
-        "          .   B@M@B@MMM@B@B@B@MMM@@@M@B                \n"
-        "              @B@B.i@MBB@B@B@@BM@::B@B@                \n"
-        "              B@@@ .B@B.:@B@ :B@B  @B@O                \n"
-        "                :0 r@B@  B@@ .@B@: P:                  \n"
-        "                    vMB :@B@ :BO7                      \n"
-        "                        ,B@B                        ```\n")
+        await self.bot.say("\n".join(["```fix"
+        "                       :PB@Bk:                         "
+        "                   ,jB@@B@B@B@BBL.                     "
+        "                7G@B@B@BMMMMMB@B@B@Nr                  "
+        "            :kB@B@@@MMOMOMOMOMMMM@B@B@B1,              "
+        "        :5@B@B@B@BBMMOMOMOMOMOMOMM@@@B@B@BBu.          "
+        "     70@@@B@B@B@BXBBOMOMOMOMOMOMMBMPB@B@B@B@B@Nr       "
+        "   G@@@BJ iB@B@@  OBMOMOMOMOMOMOM@2  B@B@B. EB@B@S     "
+        "   @@BM@GJBU.  iSuB@OMOMOMOMOMOMM@OU1:  .kBLM@M@B@     "
+        "   B@MMB@B       7@BBMMOMOMOMOMOBB@:       B@BMM@B     "
+        "   @@@B@B         7@@@MMOMOMOMM@B@:         @@B@B@     "
+        "   @@OLB.          BNB@MMOMOMM@BEB          rBjM@B     "
+        "   @@  @           M  OBOMOMM@q  M          .@  @@     "
+        "   @@OvB           B:u@MMOMOMMBJiB          .BvM@B     "
+        "   @B@B@J         0@B@MMOMOMOMB@B@u         q@@@B@     "
+        "   B@MBB@v       G@@BMMMMMMMMMMMBB@5       F@BMM@B     "
+        "   @BBM@BPNi   LMEB@OMMMM@B@MMOMM@BZM7   rEqB@MBB@     "
+        "   B@@@BM  B@B@B  qBMOMB@B@B@BMOMBL  B@B@B  @B@B@M     "
+        "    J@@@@PB@B@B@B7G@OMBB.   ,@MMM@qLB@B@@@BqB@BBv      "
+        "       iGB@,i0@M@B@MMO@E  :  M@OMM@@@B@Pii@@N:         "
+        "          .   B@M@B@MMM@B@B@B@MMM@@@M@B                "
+        "              @B@B.i@MBB@B@B@@BM@::B@B@                "
+        "              B@@@ .B@B.:@B@ :B@B  @B@O                "
+        "                :0 r@B@  B@@ .@B@: P:                  "
+        "                    vMB :@B@ :BO7                      "
+        "                        ,B@B                        ```"]))
         
     @commands.command()
     async def apologize(self):
@@ -203,13 +163,13 @@ class Randomshizzle:
         await self.bot.say("Currently {} people paid their respect using {}pressf!".format(self.pressfcount[0], ctx.prefix))
       
     @commands.command()
-    @commands.cooldown(2, 60, commands.BucketType.user)
+    @commands.cooldown(1, random.randint(1, 10), commands.BucketType.user)
     async def cancermeter(self, *, torate=None):
         """Tells you how cancerous something is."""
         if torate != None:
-            await self.bot.say("```Cancer meter for '{}':\n"
-                                "0    1    2    3    4    5    6    7    8    9   10\n"
-                                "|----|----|----|----|----|----|----|----|----|----|\n".format(torate)
+            await self.bot.say("\n".join(["```Cancer meter for '{}':".format(torate)
+                                "0    1    2    3    4    5    6    7    8    9   10"
+                                "|----|----|----|----|----|----|----|----|----|----|"])
                                 + "     " * random.randint(0, 5)+ "^```")
         else:
             await self.bot.say("```Cancer meter:\n"
@@ -218,16 +178,15 @@ class Randomshizzle:
                                 + "     " * random.randint(0, 5)+ "^```")
                                 
     @commands.command(pass_context=True)
-    async def rate(self, ctx, *, something):
+    async def rate(self, ctx, *, user:discord.Member):
         """Rate something."""
-        for mention in ctx.message.mentions:
-            if mention.id == ctx.message.server.me.id:
-                await self.bot.say("I would rate myself a 10/10.")
-                return
-            elif mention.id == self.bot.settings.owner:
-                await self.bot.say("I would rate my owner a 10/10.")
-                return
-        await self.bot.say("I would rate {} a {}/10.".format(something, random.randint(0, 10)))
+        if user.id == ctx.message.server.me.id:
+            await self.bot.say("I would rate myself a 10/10.")
+            return
+        elif user.id == self.bot.settings.owner:
+            await self.bot.say("I would rate my owner a 10/10.")
+            return
+        await self.bot.say("I would rate {} a {}/10.".format(user.mention, random.randint(0, 10)))
       
 def check_folders():
     if not os.path.exists("data/pressf"):
